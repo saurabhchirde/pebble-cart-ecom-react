e3c729909821d48a592409695a231d6ef5994c26
saurabhchirde <saurabh123.chirde@gmail.com>
2022-03-27 15:04:47 +0530

    fix: redirects on fail

--- src/App.js
+++ src/App.js
@@ -49,6 +49,7 @@ const App = () => {
           <Route path="cart" element={<CartPage />} />
           <Route path="cart/checkout" element={<CheckoutPage />} />
           <Route path="mockman" element={<Mockman />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BodyWrapper>
       <Footer />
